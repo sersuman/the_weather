6472b1c8cb2e00a8f789e94941c13eaa823ac9e0
sersuman <sumanshrestha670@gmail.com>
2020-01-17 14:22:37 +0545

    weather data received from API

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,11 @@
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    url = 'https://samples.openweathermap.org/data/2.5/weather?q={}&appid=b6907d289e10d714a6e88b30761fae22'
+    city = 'Las Vegas'
+    
+    r = requests.get(url.format(city))
+    print(r.text)
+    return render(request, 'weather/weather.html')
